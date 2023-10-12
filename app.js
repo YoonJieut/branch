7f40e2cd8f1b8ce24f9af06d5e472447ccb98cfc
Yoonjieut <qmdldos@gmail.com>
2023-10-12 10:42:56 +0900

    기본 서버 작성하기

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log('hi');
+
+const http = require('http');
+
+const server = http.createServer((req, res)=>{
+
+  
+});
+
+server.listen(3500, (err)=>{
+  if (!err){
+    console.error('서버 안 돌아감');
+  } else {
+    // ! 수퍼 꿀팁, 링크를 만들어 놓으면 눌러서 바로 넘어가면 테스트하기 편하다.
+    console.log('3500 포트에 돌아감 . http://localhost:3500');
+  }
+})
