7eb60bcb1cc6d5d74a1a61cdedcf595ab03da53b
Yoonjieut <qmdldos@gmail.com>
2023-10-12 11:48:59 +0900

    jieut 에러 전용 변경

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const server = http.createServer((req, res)=>{
   // ! 이래나 저러나 써야하는 "골격"에 쓰는 것들은 메인에 쓰긴 합니다.
   //  * 그러나 습관상, 바로 브랜치를 만들도록 합니다.
   res.writeHead(200, {"Content-Type" : "text/plain"})
-  res.write("ㅁㄴㅇ");
   res.end();
 });
 
