50bef2c396100d87ed575cba5ccd9f8ff4874898
Yoonjieut <qmdldos@gmail.com>
2023-10-12 11:36:30 +0900

    jieut 브랜치 수정

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res)=>{
   // ! 이래나 저러나 써야하는 "골격"에 쓰는 것들은 메인에 쓰긴 합니다.
   //  * 그러나 습관상, 바로 브랜치를 만들도록 합니다.
   res.writeHead(200, {"Content-Type" : "text/plain"})
-  res.write("향마루 가보자고요");
+  res.write("ㅁㄴㅇ");
   res.end();
 });
 
